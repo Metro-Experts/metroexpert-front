9283b2df916621d903f22e5e97a1975b7aa2b3d2
romancr19 <roman.chacin@correo.unimet.edu.ve>
2024-06-08 13:03:30 -0400

    feature: homepage controller

---
+++ lib/controllers/homepage_controller.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: avoid_print, use_build_context_synchronously
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:metro_experts/components/tutor_card_render.dart';
+import 'package:metro_experts/firebase_auth/auth.dart';
+import 'package:http/http.dart' as http;
+import 'package:metro_experts/model/user_model.dart';
+import 'package:provider/provider.dart';
+
+class HomePageController extends ChangeNotifier {
+  List<TutorCardRender> _tutorCard = [];
+  List<TutorCardRender> filteredTutors = [];
+  String searchQuery = '';
+
+  Future<void> fetchTutorings() async {
+    var url = Uri.parse(
+        'https://uniexpert-gateway-6569fdd60e75.herokuapp.com/courses');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      List<dynamic> responseData = json.decode(response.body);
+      _tutorCard =
+          responseData.map((data) => TutorCardRender.fromJson(data)).toList();
+      filteredTutors = _tutorCard;
+    } else {
+      print('error fetching tutorings...');
+    }
+  }
+
+  Future<void> fetchUser(BuildContext context) async {
+    var url = Uri.parse(
+        'https://uniexpert-gateway-6569fdd60e75.herokuapp.com/users/${Auth().currentUser!.uid}');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      Map responseData = json.decode(response.body);
+      Provider.of<UserOnSession>(context, listen: false)
+          .updateUserData(responseData);
+    } else {
+      print('error fetching tutorings...');
+    }
+  }
+
+  void updateSearchQuery(String query) {
+    searchQuery = query;
+    filteredTutors = _tutorCard.where((tutor) {
+      return tutor.subject.toLowerCase().contains(query.toLowerCase()) ||
+          tutor.tutorName.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+    notifyListeners();
+  }
+}
