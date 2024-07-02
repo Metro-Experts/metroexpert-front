21713075d543ab59e0206972de9055d95db74902
Hugo Duque <hugo.duque@correo.unimet.edu.ve>
2024-07-02 10:48:39 -0400

    refactor: final product

--- lib/controllers/data_payment_page_controller.dart
+++ lib/controllers/data_payment_page_controller.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: depend_on_referenced_packages
+// ignore_for_file: depend_on_referenced_packages, use_build_context_synchronously, avoid_print
 
 import 'dart:convert';
 import 'dart:io';
